d055d69636c4f7b5459d0f4d574bdc8a9dfb4b21
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-10-20 23:33:01 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -3,6 +3,6 @@
 first_number = 1
 second_number = 2
 sum = first_number + second_number
-difference = first_number - second_number 
+difference = first_number - second_number
 
 product = first_number * second_number
