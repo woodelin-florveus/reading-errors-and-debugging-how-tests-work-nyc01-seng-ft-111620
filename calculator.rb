1e5653cea81405f970bca294ab1d38942a066a9a
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-10-20 23:33:26 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -4,5 +4,5 @@ first_number = 1
 second_number = 2
 sum = first_number + second_number
 difference = first_number - second_number
-
 product = first_number * second_number
+quotient = first_number / second_number
